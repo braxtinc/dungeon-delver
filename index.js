87b27149252f9b41c2a1060bbaf939c25b2ddb9e
braxtinc <braxtinc@yahoo.com>
2019-06-24 14:59:38 -0400

    Message Handler Created and "Testing" Command created

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require('Discord.js');
+const Enmap = require('enmap');
+const fs = require('fs');
+
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.config = config;
+
+//Reading the "Events Folder" to be able to trigger said events
+
+fs.readdir('./events/', (err, files) => {
+	if (err) return console.error(err);
+	files.forEach(file => {
+		const event = require(`./events/${file}`);
+		let eventName = file.split('.')[0];
+		client.on(eventName, event.bind(null, client));
+	});
+});
+
+console.log('\nLoading Commands\n');
+
+// Grabbing Commands from the commands folder
+
+client.commands = new Enmap();
+
+fs.readdir('./commands/', (err, files) => {
+	if (err) return console.error(err);
+	files.forEach(file => {
+		if (!file.endsWith('.js')) return;
+		let props = require(`./commands/${file}`);
+		let commandName = file.split('.')[0];
+		console.log(`Loading Command ${commandName}`);
+		client.commands.set(commandName, props);
+	});
+});
+
+console.log('\nLoaded Commands\nLogging In..\n');
+
+client.login(config.token);
